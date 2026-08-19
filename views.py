b6f356ee9d30c996b233f23a2fa6df7cc347cb4d
Kevin Song <kevinmgls@al.insper.edu.br>
2026-08-18 21:01:00 -0400

    css base

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from utils import load_data, load_template
 import json
 
 def index():
-    note_template = load_template('components/note.html')
+    note_template = load_template('components/notes.html')
     notes_li = [
         note_template.format(title=dados['titulo'], details=dados['detalhes'])
         for dados in load_data('static/data/notes.json')
